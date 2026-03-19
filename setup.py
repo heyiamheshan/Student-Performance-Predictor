88a81d81aa6fbb61a079582df5b178a4c2771c9e
Heshan Pramuditha Dharmasena <rmheshanpramuditha@gmail.com>
2026-03-19 10:40:11 +0530

    add -e .

--- setup.py
+++ setup.py
@@ -1,5 +1,14 @@
     from setuptools improt find_packages,setup 
 
+    def get_requiremnts(file_path)->List[str]:
+        '''
+        This function will return the list of requirements 
+        '''
+        requirements=[]
+        with open(file)as file_obj:
+            requirements=file_obj.readlines()
+            requirements=[req.replace("\n","") for req in requirements]
+
     setup(
     name='mlproject',
     version='0.0.1',
