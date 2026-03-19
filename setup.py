d7e2283e1115b44fa191a4f7bb28518d58c3a1c1
Heshan Pramuditha Dharmasena <rmheshanpramuditha@gmail.com>
2026-03-19 13:09:39 +0530

    install requirements.txt

--- setup.py
+++ setup.py
@@ -1,19 +1,25 @@
-    from setuptools improt find_packages,setup 
+from setuptools import find_packages,setup 
 
-    def get_requiremnts(file_path)->List[str]:
-        '''
+
+HYPEN_E_DOT = "-e ."
+def get_requirements(file_path)->List[str]:
+    '''
         This function will return the list of requirements 
         '''
-        requirements=[]
-        with open(file)as file_obj:
-            requirements=file_obj.readlines()
-            requirements=[req.replace("\n","") for req in requirements]
+    requirements=[]
+    with open(file_path)as file_obj:
+        requirements=file_obj.readlines()
+        requirements=[req.replace("\n","") for req in requirements]
 
-    setup(
+    if HYPEN_E_DOT in requirements:
+        requirements.remove(HYPEN_E_DOT)
+    return requirements
+
+setup(
     name='mlproject',
     version='0.0.1',
     author='heshan',
     author_email='pramudithaheshan8@gmail.com',
     packages=find_packages(),
-    install_requires=get_requirements('requirements.txt')
+    install_requires=get_requirements('requirements.txt')               
     )
