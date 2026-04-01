13727e81013e0c60755bd700bb8b12cc2bf16ae0
Heshan Pramuditha Dharmasena <rmheshanpramuditha@gmail.com>
2026-04-01 13:33:35 +0530

    hyperparameter traninning is done

--- src/component/model_trainer.py
+++ src/component/model_trainer.py
@@ -47,7 +47,40 @@ class ModelTrainer:
                 "KNeighborsRegressor":KNeighborsRegressor()
             }
             
-            model_report:dict=evaluate_models(X_train,y_train,X_test,y_test,models)
+        
+        
+            params={
+                "DecisionTreeRegressor": {
+                    'criterion':['squared_error', 'friedman_mse', 'absolute_error', 'poisson'],
+                },
+                "RandomForestRegressor":{
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "GradientBoostingRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'subsample':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "LinearRegression":{},
+                "XGBRegressor":{
+                    'learning_rate':[.1,.01,.05,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "CatBoostRegressor":{
+                    'depth': [6,8,10],
+                    'learning_rate': [0.01, 0.05, 0.1],
+                    'iterations': [30, 50, 100]
+                },
+                "AdaBoostRegressor":{
+                    'learning_rate':[.1,.01,0.5,.001],
+                    'n_estimators': [8,16,32,64,128,256]
+                },
+                "KNeighborsRegressor": {
+                    'n_neighbors':[5,7,9,11],
+                }
+            }
+            
+            model_report:dict=evaluate_models(X_train,y_train,X_test,y_test,models,param=params)
 
             best_model_score=max(sorted(model_report.values()))
             best_model_name=list(model_report.keys())[list(model_report.values()).index(best_model_score)]
--- src/utils.py
+++ src/utils.py
@@ -6,6 +6,7 @@ import pandas as pd
 import numpy as np
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 from src.exception import CustomException
 from src.logger import logging
 
@@ -22,12 +23,20 @@ def save_object(file_path: str,obj):
         raise CustomException(e,sys)
 
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report={}
         for i in range (len (list (models ))):
-            model=list(models.items())[i][1]
+            model_name = list(models.keys())[i]
+            model = list(models.values())[i]
+            para = param.get(model_name, {})
 
+            if para:
+                gs = GridSearchCV(model, para, cv=3)
+                gs.fit(X_train, y_train)
+
+                model.set_params(**gs.best_params_)
+            
             model.fit(X_train,y_train) #train the model
 
             y_train_pred=model.predict(X_train)
@@ -35,7 +44,7 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
 
             train_model_score=r2_score(y_train,y_train_pred)
             test_model_score=r2_score(y_test,y_test_pred)
-           
+
             report[list(models.keys())[i]]=test_model_score
 
         return report
